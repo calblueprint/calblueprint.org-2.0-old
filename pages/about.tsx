1e0edfd905c21f962c69353026035721c9e7b76e
Frederick Kim <freddy.kim8@gmail.com>
2021-02-03 01:54:55 -0800

    remove layout component

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import Layout from 'components/Layout';
+
+const AboutPage: React.FC = () => {
+  return (
+    <Layout title="About | Blueprint">
+      <div>hello about page</div>
+    </Layout>
+  );
+};
+
+export default AboutPage;
