e7034a29a663afb6bf7a3616994d780640ec8e96
Frederick Kim <freddy.kim8@gmail.com>
2021-02-03 01:55:51 -0800

    Rename layout component with caps

---
+++ components/Layout.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import Link from 'next/link';
+
+type Props = {
+  title?: string;
+};
+
+const Layout: React.FC<Props> = ({ children, title = 'Blueprint' }) => {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header>
+        <nav>
+          <Link href="/">
+            <a>
+              <h1>Blueprint</h1>
+            </a>
+          </Link>
+        </nav>
+      </header>
+      {children}
+      <footer />
+    </div>
+  );
+};
+
+export default Layout;
